29ff7ce195e534fd2c785b519fc861dab11409ea
vanesa vidal <vanesa.fatima.vidal22@gmail.com>
2022-03-24 20:27:19 -0300

    primer trabajo

---
+++ CLASE1/variables.js/ejercicio1.js
@@ -0,0 +1,15 @@
+
+
+let datos = {
+    nombre: 'Vanesa',
+    edad: 28,
+    altura: 1.53,
+    peliculas: []
+}
+console.log (datos);
+
+datos.altura += 0.05;
+
+datos.peliculas.push('harry');
+
+console.log (datos)
